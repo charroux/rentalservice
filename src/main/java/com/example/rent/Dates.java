1faf10c7949e022314d14572dd2f270322dfeb14
charroux <benoit.charroux@gmail.com>
2025-08-27 15:08:11 +0200

    first commit

---
+++ src/main/java/com/example/rent/Dates.java
@@ -0,0 +1,23 @@
+package com.example.rent;
+
+public class Dates {
+
+    String begin;
+    String end;
+
+    public String getBegin() {
+        return begin;
+    }
+
+    public void setBegin(String begin) {
+        this.begin = begin;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+}
---
+++ src/main/java/com/example/rent/RentWebService.java
@@ -0,0 +1,29 @@
+package com.example.rent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RentWebService {
+
+    Logger logger = LoggerFactory.getLogger(RentWebService.class);
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello !";
+    }
+
+    @PutMapping("/cars/{plateNumber}")
+    public void rent(
+            @PathVariable("plateNumber")String plaque,
+            @RequestParam("rent")boolean toRent,
+            @RequestBody Dates dates){
+
+        logger.info("Plaque : " + plaque);
+        logger.info("rent : " + toRent);
+        logger.info("dates : " + dates);
+
+    }
+
+}
